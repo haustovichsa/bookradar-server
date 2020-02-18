9b6008a694cf5e753fd9b68da57de693fa6ba504
Sergey_Khaustovich <haustovichsa@gmail.com>
2020-02-18 10:31:00 +0300

    Init commit

---
+++ tests/user.test.js
@@ -0,0 +1,72 @@
+import 'cross-fetch/polyfill'
+import '@babel/polyfill/noConflict'
+import prisma from '../src/prisma'
+import seedDatabase, { userOne } from './utils/seedDatabase'
+import getClient from './utils/getClient'
+import { createUser, login, getUsers, getProfile } from './utils/operations'
+
+const client = getClient()
+
+beforeEach(seedDatabase)
+
+test('Should create a new user', async () => {
+    const variables = {
+        data: {
+            name: 'Sergey Chan',
+            email: 'SergeyChan@test.test',
+            password: 'Qwerty12'    
+        }
+    }
+   
+    const responce = await client.mutate({ mutation: createUser, variables })
+
+    const exists = await prisma.exists.User({ id: responce.data.createUser.user.id })
+
+    expect(exists).toBe(true)
+
+})
+
+test('Should expose public author profile', async () => {
+   const responce = await client.query({ query: getUsers })
+
+    expect(responce.data.users.length).toBe(2)
+    expect(responce.data.users[0].email).toBe(null)
+    expect(responce.data.users[0].name).toBe("Pavel Chan")
+})
+
+test('should not login with bad credentials', async () => {
+    const variables = {
+        data: {
+            email: 'PavelChan@gmail.com',
+            password: 'testpassword'
+        }
+    }
+
+    await expect(
+        client.mutate({ mutation: login, variables })   
+    ).rejects.toThrow()
+})
+
+test('Should not signup user with invalid password', async () => {
+    const variables = {
+        data: {
+            name: 'Nina',
+            email: 'NinaChan@gmail.com',
+            password: 'pass'
+        }
+    }
+
+    await expect(
+        client.mutate({ mutation: createUser, variables })
+    ).rejects.toThrow()
+})  
+
+test('should fetch user profile', async () => {
+    const client = getClient(userOne.jwt)
+
+    const { data } = await client.query({ query: getProfile })
+
+    expect(data.me.id).toBe(userOne.user.id)
+    expect(data.me.name).toBe(userOne.user.name)
+    expect(data.me.email).toBe(userOne.user.email)
+})
---
+++ tests/utils/seedDatabase.js
@@ -0,0 +1,40 @@
+import bcrypt from 'bcryptjs'
+import prisma from '../../src/prisma'
+import jwt from 'jsonwebtoken'
+
+const userOne = {
+    input: {
+        name: 'Pavel Chan',
+        email: 'PavelChan@gmail.com',
+        password: bcrypt.hashSync('Qwerty12')
+    },
+    user: undefined,
+    jwt: undefined
+}
+
+const userTwo = {
+    input: {
+        name: 'Andrey Chan',
+        email: 'AndreyChan@gmail.com',
+        password: bcrypt.hashSync('Qwerty12')
+    },
+    user: undefined,
+    jwt: undefined
+}
+
+const seedDatabase = async () => {
+    // delete test data
+    await prisma.mutation.deleteManyUsers()
+    
+    // create user one
+    userOne.user = await prisma.mutation.createUser({ data: userOne.input })
+
+    userOne.jwt = jwt.sign({ userId: userOne.user.id }, process.env.JWT_SECRET)
+
+    // create user two
+    userTwo.user = await prisma.mutation.createUser({ data: userTwo.input })
+
+    userTwo.jwt = jwt.sign({ userId: userTwo.user.id }, process.env.JWT_SECRET)
+}
+
+export { seedDatabase as default, userOne }
